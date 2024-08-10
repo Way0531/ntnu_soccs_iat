6b1ac27f648cc44372ce5f6e7bb4c91c534b9cf5
Wei-Yu Lin <61001030e@gapps.ntnu.edu.tw>
2024-08-11 01:01:47 +0800

    Update demographic.js

--- demographic.js
+++ demographic.js
@@ -1,74 +1,24 @@
 define(['questAPI'], function(Quest){
     var API = new Quest();
 
-    // ### Questions
-    API.addQuestionsSet('basicSelect',
-    {
-        type: 'selectOne',
-        autoSubmit:false,
-        numericValues:true,
-        required:false,
-        style:'multiButtons',
-        answers : [
-            '1 - Extremely negative',
-            '2',
-            '3',
-            '4',
-            '5',
-            '6',
-            '7',
-            '8',
-            '9 - Extremely positive'
-        ]
-    });
-
-    API.addQuestionsSet('people',
-    [
+    API.addSequence([
         {
-            inherit : 'basicSelect',
-            name : 'Obama',
-            stem : 'Barack Obama'
-        },
-        {
-            inherit : 'basicSelect',
-            name : 'Beyonce',
-            stem : 'Beyonce Knowles'
-        },
-        {
-            inherit : 'basicSelect',
-            name : 'Colbert',
-            stem : 'Stephen Colbert'
-        },
-        {
-            inherit : 'basicSelect',
-            name : 'Letterman',
-            stem : 'David Letterman'
+            questions : {
+                type: 'selectOne',
+                style:'multiButtons',
+                errorMsg: {
+                    required: 'Please select an answer, or click \'decline to answer\''
+                },
+                
+                name:'item1',
+                stem : 'On the whole, I am satisfied with myself.',
+                answers : ['Strongly Disagree', 'Disagree', 'Agree', 'Strongly Agree'],
+                
+                name:'item2',
+                stem : 'On the whole, I am satisfied with myself.',
+                answers : ['Strongly Disagree', 'Disagree', 'Agree', 'Strongly Agree']
+            }
         }
     ]);
-
-    // ### Pages
-    // Shows all four questions in a fixed order.
-    API.addPagesSet('basicPage',
-    {
-        //progressBar: '<%= pagesMeta.number %> out of 4',
-        header: 'How positive or negative are your feelings toward the people listed below?',
-        headerStyle : {'font-size':'1em'},
-        questions : [
-            {inherit:{set:'people', type:'all'}}
-        ],
-        v1style:2,
-        decline:false,
-        numbered: false
-    });
-
-    // ### Sequence
-    API.addSequence(
-    [
-        {inherit : 'basicPage'}
-    ]);
-
-    /**
-    Return the script to piquest's god, or something of that sort.
-    **/
     return API.script;
 });

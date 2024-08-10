0e138f855a12f98d609e062e7f538a103d971aab
Wei-Yu Lin <61001030e@gapps.ntnu.edu.tw>
2024-08-11 01:10:04 +0800

    Update demographic.js

--- demographic.js
+++ demographic.js
@@ -1,24 +1,78 @@
 define(['questAPI'], function(Quest){
-    var API = new Quest();
+	var API = new Quest();
 
-    API.addSequence([
-        {
-            questions : {
-                type: 'selectOne',
-                style:'multiButtons',
-                errorMsg: {
-                    required: 'Please select an answer, or click \'decline to answer\''
-                },
-                
-                name:'item1',
-                stem : 'On the whole, I am satisfied with myself.',
-                answers : ['Strongly Disagree', 'Disagree', 'Agree', 'Strongly Agree'],
-                
-                name:'item2',
-                stem : 'On the whole, I am satisfied with myself.',
-                answers : ['Strongly Disagree', 'Disagree', 'Agree', 'Strongly Agree']
-            }
-        }
-    ]);
-    return API.script;
+	// ### Questions
+	API.addQuestionsSet('basicSelect',
+	{
+		type: 'selectOne',
+		autoSubmit:false,
+		numericValues:true,
+		required:false,
+		style:'multiButtons',
+		answers : [
+			'1 - Extremely negative',
+			'2',
+			'3',
+			'4',
+			'5',
+			'6',
+			'7',
+			'8',
+			'9 - Extremely positive'
+		]
+	});
+
+	API.addQuestionsSet('people',
+	[
+		{
+			inherit : 'basicSelect',
+			name : 'Obama',
+			stem : 'Barack Obama'
+		},
+		{
+			inherit : 'basicSelect',
+			name : 'Beyonce',
+			stem : 'Beyonce Knowles'
+		},
+		{
+			inherit : 'basicSelect',
+			name : 'Colbert',
+			stem : 'Stephen Colbert'
+		},
+		{
+			inherit : 'basicSelect',
+			name : 'Letterman',
+			stem : 'David Letterman'
+		}
+	]);
+
+	// ### Pages
+	// Shows all four questions, but the order is random.
+	API.addPagesSet('basicPage',
+	{
+		progressBar: '<%= pagesMeta.number %> out of 4',
+		header: 'How positive or negative are your feelings toward the people listed below?',
+		headerStyle : {'font-size':'1em'},
+		questions : {
+			mixer : 'repeat',
+			times : 4,
+			data : [
+				{inherit:{set:'people', type:'exRandom'}}
+			]
+		},
+		v1style:2,
+		decline:false,
+		numbered: false
+	});
+
+	// ### Sequence
+	API.addSequence(
+	[
+		{inherit : 'basicPage'}
+	]);
+
+	/**
+	Return the script to piquest's god, or something of that sort.
+	**/
+	return API.script;
 });

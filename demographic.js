a6762a689e4c2b65139224945a15154e33c68cab
Wei-Yu Lin <61001030e@gapps.ntnu.edu.tw>
2024-08-11 00:29:59 +0800

    Update demographic.js

--- demographic.js
+++ demographic.js
@@ -1,88 +1,77 @@
 define(['questAPI'], function(Quest){
-	var API = new Quest();
+    var API = new Quest();
 
-	// ### Questions
-	API.addQuestionsSet('basicSelect',
-	{
-		type: 'selectOne',
-		autoSubmit:false,
-		numericValues:true,
-		required:false,
-		style:'multiButtons',
-		answers : [
-			'1 - Extremely negative',
-			'2',
-			'3',
-			'4',
-			'5',
-			'6',
-			'7',
-			'8',
-			'9 - Extremely positive'
-		]
-	});
+    // ### Questions
+    API.addQuestionsSet('basicSelect',
+    {
+        type: 'selectOne',
+        autoSubmit:false,
+        numericValues:true,
+        required:false,
+        style:'multiButtons',
+        answers : [
+            '1 - Extremely negative',
+            '2',
+            '3',
+            '4',
+            '5',
+            '6',
+            '7',
+            '8',
+            '9 - Extremely positive'
+        ]
+    });
 
-	API.addQuestionsSet('people',
-	[
-		{
-			inherit : 'basicSelect',
-			name : 'Obama',
-			stem : '1'
-		},
-		{
-			inherit : 'basicSelect',
-			name : 'Beyonce',
-			stem : '2'
-		},
-		{
-			inherit : 'basicSelect',
-			name : 'Colbert',
-			stem : '3'
-		},
-		{
-			inherit : 'basicSelect',
-			name : 'Letterman',
-			stem : '4'
-		},
-		{
-			inherit : 'basicSelect',
-			name : 'five',
-			stem : '5'
-		},
-		{
-			inherit : 'basicSelect',
-			name : 'six',
-			stem : '6'
-		}
-	]);
+    API.addQuestionsSet('people',
+    [
+        {
+            inherit : 'basicSelect',
+            name : 'Obama',
+            stem : 'Barack Obama'
+        },
+        {
+            inherit : 'basicSelect',
+            name : 'Beyonce',
+            stem : 'Beyonce Knowles'
+        },
+        {
+            inherit : 'basicSelect',
+            name : 'Colbert',
+            stem : 'Stephen Colbert'
+        },
+        {
+            inherit : 'basicSelect',
+            name : 'Letterman',
+            stem : 'David Letterman'
+        }
+    ]);
 
-	// ### Pages
-	// Shows all four questions, but the order is random.
-	API.addPagesSet('basicPage',
-	{
-		//progressBar: '<%= pagesMeta.number %> out of 4',
-		header: '基本資訊',
-		headerStyle : {'font-size':'1em'},
-		questions : {
-			//mixer : 'repeat',
-			//times : 6,
-			data : [
-				{inherit:{set:'people', type:'custom'}}
-			]
-		},
-		v1style:2,
-		decline:false,
-		numbered: false
-	});
+    // ### Pages
+    // Shows all four questions in a fixed order.
+    API.addPagesSet('basicPage',
+    {
+        progressBar: '<%= pagesMeta.number %> out of 4',
+        header: 'How positive or negative are your feelings toward the people listed below?',
+        headerStyle : {'font-size':'1em'},
+        questions : [
+            {inherit:{set:'people', type:'single', name:'Obama'}},
+            {inherit:{set:'people', type:'single', name:'Beyonce'}},
+            {inherit:{set:'people', type:'single', name:'Colbert'}},
+            {inherit:{set:'people', type:'single', name:'Letterman'}}
+        ],
+        v1style:2,
+        decline:false,
+        numbered: false
+    });
 
-	// ### Sequence
-	API.addSequence(
-	[
-		{inherit : 'basicPage'}
-	]);
+    // ### Sequence
+    API.addSequence(
+    [
+        {inherit : 'basicPage'}
+    ]);
 
-	/**
-	Return the script to piquest's god, or something of that sort.
-	**/
-	return API.script;
+    /**
+    Return the script to piquest's god, or something of that sort.
+    **/
+    return API.script;
 });

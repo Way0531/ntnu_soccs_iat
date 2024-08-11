881cd078396be1d688c11f45531b2e38aa7f9071
Wei-Yu Lin <61001030e@gapps.ntnu.edu.tw>
2024-08-11 12:44:04 +0800

    Update explicits.js

--- explicits.js
+++ explicits.js
@@ -1,102 +1,13 @@
 define(['questAPI'], function(Quest){
-    let API = new Quest();
-    let isTouch = API.getGlobal().$isTouch;
-	
-    /**
-	* Page prototype
-	*/
-    API.addPagesSet('basicPage',{
-        noSubmit:false, //Change to true if you don't want to show the submit button.
-        header: 'Questionnaire',
-        decline: false,
-        declineText: isTouch ? 'Decline' : 'Decline to Answer', 
-        autoFocus:true, 
-        progressBar:  'Page <%= pagesMeta.number %> out of 3'
-    });
-
-    /**
-	* Question prototypes
-	*/
-    API.addQuestionsSet('basicQ',{
-        decline: false,
-        required : true, 		
-        errorMsg: {
-            required: isTouch 
-                ? '此題項為必填' 
-                : '此題項為必填'
-        },
-        autoSubmit: true,
-        numericValues: true,
-        help: '<%= pagesMeta.number < 3 %>',
-        helpText: 'Tip: For quick response, click to select your answer, and then click again to submit.'
-    });
-
-    API.addQuestionsSet('basicSelect',{
-        inherit :'basicQ',
-        type: 'selectOne'
-    });
-	
-    API.addQuestionsSet('basicDropdown',{
-        inherit :'basicQ',
-        type : 'dropdown',
-        autoSubmit: false
-    });
-	
-    API.addQuestionsSet('therm',{
-        inherit: 'basicSelect',
-        answers: [
-            {text:'10 - Extremely warm', value:10},
-            {text:'9 - Very warm', value:9},
-            {text:'8 - Moderately warm', value:8},
-            {text:'7 - Somewhat warm', value:7},
-            {text:'6 - Slightly warm', value:6},
-            {text:'5 - Neither warm nor cold', value:5},
-            {text:'4 - Slightly cold', value:4},
-            {text:'3 - Somewhat cold', value:3},
-            {text:'2 - Moderately cold', value:2},
-            {text:'1 - Very cold', value:1},
-            {text:'0 - Extremely cold', value:0}
-        ]
-    });
-
-	
-    /**
-	*Specific questions
-	*/	
-    API.addQuestionsSet('attributes7',{
-        inherit : 'basicSelect',
-        name: 'attributes7',
-        stem: 'Which statement best describes you?',
-        answers: [
-            {text:'I strongly prefer <%= global.whiteLabels %> to <%= global.blackLabels %>.',value:7},
-            {text:'I moderately prefer <%= global.whiteLabels %> to <%= global.blackLabels %>.',value:6},
-            {text:'I slightly prefer <%= global.whiteLabels %> to <%= global.blackLabels %>.',value:5},
-            {text:'I like <%= global.whiteLabels %> and <%= global.blackLabels %> equally.',value:4},
-            {text:'I slightly prefer <%= global.blackLabels %> to <%= global.whiteLabels %>.',value:3},
-            {text:'I moderately prefer <%= global.blackLabels %> to <%= global.whiteLabels %>.',value:2},
-            {text:'I strongly prefer <%= global.blackLabels %> to <%= global.whiteLabels %>.',value:1}
-        ]
-    });
-	
-    API.addQuestionsSet('thermBlack',{
-        inherit : 'therm',
-        name: 'Tblack_0to10',
-        stem: 'How warm or cold do you feel towards <b><%= global.blackLabels %></b>?'
-    });
-
-    API.addQuestionsSet('thermWhite',{
-        inherit : 'therm',
-        name: 'Twhite_0to10',
-        stem: 'How warm or cold do you feel towards <b><%= global.whiteLabels %></b>?'
-    });
+    var API = new Quest();
 
     API.addSequence([
-	     { // page begins
-            header: '性傾向諮商能力量表',
+        { // page begins
+            header: 'Grid questions simple',
             questions: [
                 {
                     type: 'grid',
-                    stem: '請勾選最符合自己的形容',
+                    stem: 'What sort of things do you like?',
                     columns: ['非常不符合' , '不符合' , '有點不符合' , '普通' , '有點符合' , '符合' , '非常符合'],
                     rows: [
 '1. 我已經具備足夠的臨床訓練與督導，可以提供同志個案諮商服務。', 
@@ -130,17 +41,31 @@ define(['questAPI'], function(Quest){
 '29. 談到同志，我認同以下的說法：「厭惡或譴責那罪，但愛那些罪人。」'
 ]
                 }
-            ]}
-        {
-            mixer : 'random', 
-            data : [
+            ]
+        }, // page ends
+        { // page begins
+            header: 'Grid questions advanced',
+            questions: [
                 {
-                    mixer : 'random', 
-                    wrapper:true, 
-                    data : [
-                        {
-                            inherit:'basicPage', 
-                            questions: {inherit:'thermBlack'}
-                        },
-                        {
-                           
+                    type: 'grid',
+                    name:'grid',
+                    columns: [
+                        'Strongly agree',
+                        'agree',
+                        'don\'t know',
+                        'disagree',
+                        'Strongly disagree',
+                        {stem:'Decline to answer', value:'n/a', noReverse:true}
+                    ],
+                    rows: [
+                        {stem:'I like grids',name:'likeGrids'},
+                        // This questions scores will be reversed so that the sum of scores is meaningful
+                        {stem:'I hate bananas', name:'likeBananas', reverse:true}
+                    ]
+                }
+            ]
+        } // page ends
+    ]);
+
+    return API.script;
+});

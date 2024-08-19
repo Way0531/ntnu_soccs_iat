50b86805ada202f3a1cd19ac9471b53338652c52
Wei-Yu Lin <61001030e@gapps.ntnu.edu.tw>
2024-08-19 20:57:44 +0800

    Update demographic.js

--- demographic.js
+++ demographic.js
@@ -17,7 +17,7 @@ define(['questAPI'], function(Quest){
 			type: 'selectOne',
 			inherit : 'basicSelect',
 			name : 'first_time',
-			stem : '請問這是否是您第一次參與本測驗？',
+			stem : '1. 請問這是否是您第一次參與本測驗？',
 			answers : [
 			'是',
 			'否']
@@ -26,7 +26,7 @@ define(['questAPI'], function(Quest){
 			type: 'selectOne',
 			inherit : 'basicSelect',
 			name : 'gender',
-			stem : '請問您的生理性別是？',
+			stem : '2. 請問您的生理性別是？',
 			answers : [
 			'男性',
 			'女性',
@@ -36,7 +36,7 @@ define(['questAPI'], function(Quest){
 			type: 'selectOne',
 			inherit : 'basicSelect',
 			name : 'sexuality',
-			stem : '請問您自身的性傾向認同是？',
+			stem : '3. 請問您自身的性傾向認同是？',
 			answers : [
 			'異性戀',
 			'同性戀',
@@ -47,7 +47,7 @@ define(['questAPI'], function(Quest){
 			type:'dropdown', 
 			inherit : 'basicSelect',
 			name : 'age',
-			stem : '請問您的年齡是？(以填表當天足歲計算）',
+			stem : '4. 請問您的年齡是？(以填表當天足歲計算）',
 			answers : [
 			'21','22','23','24','25','26','27','28','29','30',
 			'31','32','33','34','35','36','37','38','39','40',
@@ -62,7 +62,7 @@ define(['questAPI'], function(Quest){
 			type: 'selectOne',
 			inherit : 'basicSelect',
 			name : 'religion',
-			stem : '請問您是否有宗教信仰？',
+			stem : '5. 請問您是否有宗教信仰？',
 			answers : [
 			'無信仰',
 			'基督教',
@@ -76,7 +76,7 @@ define(['questAPI'], function(Quest){
 			type: 'selectOne',
 			inherit : 'basicSelect',
 			name : 'job',
-			stem : '請問您目前的身份是？',
+			stem : '6. 請問您目前的身份是？',
 			answers : [
 			'正在進行全職實習',
 			'已完成全職實習，尚未考取諮商心理師執照',
@@ -86,7 +86,7 @@ define(['questAPI'], function(Quest){
 			type: 'selectOne',
 			inherit : 'basicSelect',
 			name : 'job_place',
-			stem : '請問您目前或最近一個執業登記/全職實習場域是？',
+			stem : '7. 請問您目前或最近一個執業登記/全職實習場域是？',
 			answers : [
 			'國小',
 			'國中/高中',
@@ -99,7 +99,7 @@ define(['questAPI'], function(Quest){
 			numericValues: true,
 			inherit : 'basicSelect',
 			name : 'job_age',
-			stem : '請問您的執業年資為？（包含全職實習）',
+			stem : '8. 請問您的執業年資為？（包含全職實習）',
 			answers : [
 			'1年（或未滿1年）','2年','3年','4年','5年','6年','7年','8年','9年','10年',
 			'11年','12年','13年','14年','15年','16年','17年','18年','19年','20年',
@@ -111,7 +111,7 @@ define(['questAPI'], function(Quest){
 			type: 'selectOne',
 			inherit : 'basicSelect',
 			name : 'training',
-			stem : '你問您是否曾於碩士班在學或是執業期間，參與過同志議題相關之課程或專業訓練？',
+			stem : '9. 你問您是否曾於碩士班在學或是執業期間，參與過同志議題相關之課程或專業訓練？',
 			answers : [
 			'是','否']
 		},
@@ -120,7 +120,7 @@ define(['questAPI'], function(Quest){
 			numericValues: true,
 			inherit : 'basicSelect',
 			name : 'self_assessment',
-			stem : '請問您認為自身目前提供同志諮商之專業勝任能力為何？',
+			stem : '10. 請問您認為自身目前提供同志諮商之專業勝任能力為何？',
 			answers : [
 			'1 - 非常不足','2 - 不足','3 - 有點不足','4 - 普通','5 - 有點足夠','6 - 足夠','7 - 非常足夠']
 		},

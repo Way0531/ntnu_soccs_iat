c9c21540e8921eaf2f2c28fdc41e86cfc328c71b
Wei-Yu Lin <61001030e@gapps.ntnu.edu.tw>
2024-09-05 13:50:37 +0800

    Update suggestion.js

--- suggestion.js
+++ suggestion.js
@@ -9,7 +9,7 @@ define(['questAPI'], function(Quest){
                     type: 'textarea',
                     stem: '請問針對目前國內心理與諮商專業領域中，同志相關議題之訓練與繼續教育，您是否有什麼相關建議與看法？（非必填問題）',
                     rows: 3,
-                    minLength: 400
+                    //minLength: 400
                 } // question ends
             ]
         } // page ends

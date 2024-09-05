256f1aa7d538099278c1c28b110ef59a857cd706
Wei-Yu Lin <61001030e@gapps.ntnu.edu.tw>
2024-09-05 13:00:36 +0800

    Update explicits.js

--- explicits.js
+++ explicits.js
@@ -10,7 +10,7 @@ define(['questAPI'], function(Quest){
         required: true, // 強制回答問題
         style: 'multiButtons',
         answers: [
-            '1 - -非常不認同',
+            '1 - 非常不認同',
             '2',
             '3',
             '4',

a75769a8e9406fdc6a171a2ce51ffc0dcc43c29a
Wei-Yu Lin <61001030e@gapps.ntnu.edu.tw>
2024-09-07 15:04:56 +0800

    Update data_upload.js

--- data_upload.js
+++ data_upload.js
@@ -10,8 +10,11 @@ function init_data_pipe(t, e, n = false) {
 
     var d = t.getGlobal();
     d.sessionId = l;
+
+    // 儲存所有階段數據的容器
     d.allData = [];
 
+    // 初始化數據管道
     fetch("https://psych-studies.com/datapipe/" + (i ? "debug/" : "") + e.split("").map(t => t.charCodeAt(0)).reduce((t, e) => t + ((t << 7) + (t << 3)) ^ e).toString(16));
 
     t.addSettings("logger", {
@@ -31,6 +34,7 @@ function init_data_pipe(t, e, n = false) {
             i.logs.push(n);
         },
         onEnd: function(t, e, n) {
+            // 收集所有階段的數據
             d.allData.push(n.logs);
             return n.logs;
         },
@@ -38,7 +42,6 @@ function init_data_pipe(t, e, n = false) {
             return e;
         },
         send: function(n, i, o, s) {
-            console.log("準備上傳資料", d.allData); // 調試資訊
             let r = "";
             if (a === "csv") {
                 r = toCsv(pivot(d.allData.flat()));
@@ -47,7 +50,7 @@ function init_data_pipe(t, e, n = false) {
             } else if (a === "json") {
                 r = JSON.stringify(d.allData);
             }
-            if (r && s.type === "experiment") { // 確保只有在所有階段結束後才上傳
+            if (r) { // 確保有數據要上傳
                 d.sent = false;
                 fetch("https://pipe.jspsych.org/api/data/", {
                     method: "POST",
@@ -60,18 +63,11 @@ function init_data_pipe(t, e, n = false) {
                         filename: "all_data_" + l + "." + a,
                         data: r
                     })
-                }).then(response => {
-                    if (response.ok) {
-                        d.sent = true;
-                        console.log("資料上傳成功");
-                    } else {
-                        console.error("上傳資料時發生錯誤:", response.statusText);
-                    }
-                }).catch(error => {
-                    console.error("上傳資料時發生錯誤:", error);
+                }).then(() => {
+                    d.sent = true;
+                }).catch((error) => {
+                    console.error("Upload failed:", error);
                 });
-            } else {
-                console.log("上傳條件不滿足：", r, s.type);
             }
         }
     });
@@ -79,10 +75,10 @@ function init_data_pipe(t, e, n = false) {
 
 // 生成上傳中的提示文本
 function generate_uploading_text(t, e, n) {
-    return "<%    wait4data();   function wait4data() {        if (document.getElementById('next_but') !== null && (global.sent === undefined || global.sent))            {return "+!!n+" ? document.getElementById('next_but').disabled = false :  document.getElementById('next_but').click();}        setTimeout(wait4data, 500);   }%>" +
+    return "<%    wait4data();   function wait4data() {        if (document.getElementById('next_but') !== null && (global.sent === undefined || global.sent))            {return " + !!n + " ? document.getElementById('next_but').disabled = false :  document.getElementById('next_but').click();}        setTimeout(wait4data, 500);   }%>" +
         (t ? "<div class='panel panel-info' style='margin-top:1em'> <div class='panel-heading'> <h1 class='panel-title' style='font-size:2em'>" + t + " </h1></div>" : "") +
         (e ? "<div class='panel-body'> <p class='lead'>" + e + "</p>" : "") +
-        ("<div class='text-center proceed' " + (n ? "" : "hidden") + " style='margin: 30px auto 10px;'><button pi-message-done type='button' " + (n ? "disabled" : "") + " id = 'next_but' class='btn btn-primary'>" + n + "</button></div>");
+        ("<div class='text-center proceed' " + (n ? "" : "hidden") + " style='margin: 30px auto 10px;'><button pi-message-done type='button' " + (n ? "disabled" : "") + " id='next_but' class='btn btn-primary'>" + n + "</button></div>");
 }
 
 // 生成上傳任務的提示
@@ -96,6 +92,6 @@ function uploading_task(t = false) {
         template: generate_uploading_text(a, i, o),
         title: n,
         name: e,
-        type: "message"
+        type: "message" // 確保這裡的 type 是正確的
     }];
 }

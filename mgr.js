19e1c7ca53b9202fec69e618d5f176b1886574ce
Wei-Yu Lin <61001030e@gapps.ntnu.edu.tw>
2024-09-08 13:38:28 +0800

    Update mgr.js

--- mgr.js
+++ mgr.js
@@ -1,27 +1,18 @@
 define(['managerAPI',
-	//'https://cdn.jsdelivr.net/gh/minnojs/minno-datapipe@1.*/datapipe.min.js'
-	'data_upload.js'
-       ], function(Manager){
-
-
+		'https://cdn.jsdelivr.net/gh/minnojs/minno-datapipe@1.*/datapipe.min.js'], function(Manager){
 	//You can use the commented-out code to get parameters from the URL.
 	//const queryString = window.location.search;
     //const urlParams = new URLSearchParams(queryString);
     //const pt = urlParams.get('pt');
-
 	var API    = new Manager();
 	//const subid = Date.now().toString(16)+Math.floor(Math.random()*10000).toString(16);
 	init_data_pipe(API, 'dd7NaZnD7IoL',  {file_type:'csv'});	
-
-
     API.setName('mgr');
     API.addSettings('skip',true);
-
     //Randomly select which of two sets of category labels to use.
     let raceSet = API.shuffle(['a','b'])[0];
     let blackLabels = [];
     let whiteLabels = [];
-
     if (raceSet == 'a') {
         blackLabels.push('同性戀');
         whiteLabels.push('異性戀');
@@ -29,7 +20,6 @@ define(['managerAPI',
         blackLabels.push('同性戀者');
         whiteLabels.push('異性戀者');
     }
-
     API.addGlobal({
         raceiat:{},
         //YBYB: change when copying back to the correct folder
@@ -49,13 +39,11 @@ define(['managerAPI',
         ]),
 	
     });
-
     API.addTasksSet({
         instructions: [{
             type: 'message',
             buttonText: '繼續'
         }],
-
         intro: [{
             inherit: 'instructions',
             name: 'intro',
@@ -63,7 +51,6 @@ define(['managerAPI',
             title: '心理師性傾向內隱態度與性傾向諮商勝任能力相關研究',
             header: '歡迎'
         }],
-
         raceiat_instructions: [{
             inherit: 'instructions',
             name: 'raceiat_instructions',
@@ -71,31 +58,26 @@ define(['managerAPI',
             //title: 'IAT Instructions',
             header: '內隱聯結測驗'
         }],
-
         explicits: [{
             type: 'quest',
             name: 'explicits',
             scriptUrl: 'explicits.js'
         }],
-
 	demographic: [{    
 		type: 'quest',
 		name: 'demographic',
 		scriptUrl: 'demographic.js'
 	}],
-
         raceiat: [{
             type: 'time',
             name: 'raceiat',
             scriptUrl: 'raceiat.js'
         }],
-
 	suggestion: [{    
 		type: 'quest',
 		name: 'suggestion',
 		scriptUrl: 'suggestion.js'
 	}],    
-
         lastpage: [{
             type: 'message',
             name: 'lastpage',
@@ -116,12 +98,14 @@ define(['managerAPI',
 		//This task waits until the data are sent to the server.
         uploading: uploading_task({header: '請稍等', body:'測驗資料上傳中，請先不要關閉頁面，謝謝！'})
     });
-
     API.addSequence([
         { type: 'isTouch', 
 	 text: '請問您正在使用智慧型手機/平板進行測驗嗎？',
 	 yesText: '是',
 	 noText: '否'}, //Use Minno's internal touch detection mechanism. 
+
+        //這邊是上傳資料
+        { type: 'post', path: ['$isTouch', 'raceSet', 'blackLabels', 'whiteLabels'] },
 
         // apply touch only styles
         {
@@ -140,7 +124,6 @@ define(['managerAPI',
                         '[pi-quest]::after {clear: both;}',
                         '[pi-quest] h3 { border-bottom: 1px solid transparent; border-top-left-radius: 3px; border-top-right-radius: 3px; padding: 10px 15px; color: inherit; font-size: 2em; margin-bottom: 20px; margin-top: 0;background-color: #d9edf7;border-color: #bce8f1;color: #31708f;}',
                         '[pi-quest] .form-group > label {font-size:1.2em; font-weight:normal;}',
-
                         '[pi-quest] .btn-toolbar {margin:15px;float:none !important; text-align:center;position:relative;}',
                         '[pi-quest] [ng-click="decline($event)"] {position:absolute;right:0;bottom:0}',
                         '[pi-quest] [ng-click="submit()"] {width:30%;line-height: 1.3333333;border-radius: 6px;}',
@@ -165,7 +148,6 @@ define(['managerAPI',
             mixer:'random',
             data:[
                 {inherit: 'explicits'},
-
                 // force the instructions to preceed the iat
                 {
                     mixer: 'wrapper',
@@ -178,10 +160,10 @@ define(['managerAPI',
         },
 	{inherit: 'suggestion'}, 
 	{inherit: 'uploading'},
-	{type: 'post', path: ['$isTouch', 'raceSet', 'blackLabels', 'whiteLabels'] }, //上傳資料
+	//改掉
+	//{type: 'post', path: ['$isTouch', 'raceSet', 'blackLabels', 'whiteLabels'] },
         {inherit: 'lastpage'},
         {inherit: 'redirect'}
     ]);
-
     return API.script;
 });

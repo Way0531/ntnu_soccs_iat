2cbbc08bdffca6e0d5ed90e0623204e136816962
Wei-Yu Lin <61001030e@gapps.ntnu.edu.tw>
2024-09-09 15:16:32 +0800

    Update demo_comp.js

--- demo_comp.js
+++ demo_comp.js
@@ -95,7 +95,7 @@ define(['questAPI'], function(Quest) {
         type: 'text',
         //inherit : 'basicSelectOld',
 	name : 'job',
-	stem : '7. 您的現職是？<br><span style="color:gray; font-size:0.8em;">例如：專任心理師/行動心理師/教師/學生/待業中</span>',
+	stem : '7. 您的現職是？<br><span style="color:gray; font-size:0.8em;">請自行輸入，例如：專任心理師/行動心理師/教師/學生/待業中</span>',
 	rows: 3,	
       },
 	    {
